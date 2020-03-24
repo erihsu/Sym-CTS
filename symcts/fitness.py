eaadd640d110f173034188e343206a6988e0c43a
xuzhenyu69 <zhenyuxu163@163.com>
2020-03-24 19:50:30 +0800

    fix unit conflict in fitness computing

--- symcts/fitness.py
+++ symcts/fitness.py
@@ -2,38 +2,66 @@
 import load_lut as load
 import numpy as np
 import os
+import json
 
 class Fitness:
-	def __init__(self,strategy):
-		self.strategy = strategy
+	def __init__(self):
 		self.prepareLut()
 		self.readClockpath()
 		self.readConfig()
 
 	def readConfig(self):
-		self.f1 = np.array([5, 10, 20, 40, 80])  # reflect between buffer size and buffer input capacitance
-		self.max_buffer = len(self.f1)
-		self.sink_cap = 15
-		with open('{}/symcts/config.txt'.format(os.getenv('SYMCTS')),'r') as f:
-			self.a     = float(f.readline().split(' ')[2])
-			self.b     = float(f.readline().split(' ')[2])
-			self.c0    = float(f.readline().split(' ')[2])
-			self.rho_f = float(f.readline().split(' ')[2])
-			self.Slin  = float(f.readline().split(' ')[2])
+
+		self.f1 = [] # reflect between buffer size and buffer input capacitance
+
+		with open('{}/utils/buffer.json'.format(os.getenv('SYMCTS')),'r') as f:
+			a_dict = json.loads(f.read())
+			self.lib_size = a_dict['buffer_num']
+			for i in range(self.lib_size):
+				self.f1.append(a_dict['buffers']['buf{}'.format(i)]['input_cap'])
+
+		with open('{}/utils/sink.json'.format(os.getenv('SYMCTS')),'r') as f:
+			a_dict = json.loads(f.read())
+			self.sink_cap = a_dict['sink']['input_cap']
+
+		with open('{}/utils/wire.json'.format(os.getenv('SYMCTS')),'r') as f:
+			a_dict = json.loads(f.read())
+			self.c0 = a_dict['wire']['unit_cap']['value']
+
+		with open('{}/utils/settings.json'.format(os.getenv('SYMCTS')),'r') as f:
+			a_dict = json.loads(f.read())
+			self.a     = a_dict['symcts']['a']
+			self.b     = a_dict['symcts']['b']
+			self.Slin  = a_dict['symcts']['Slin']
+
+		self.rho_matrix = np.load('{}/utils/rho_matrix.npy'.format(os.getenv('SYMCTS')))
 
 	def readClockpath(self):
 		self.num = []
+		self.branch = []
 		self.WL = []
 		with open('{}/symcts/clockpath.txt'.format(os.getenv('SYMCTS')),'r') as f:
 			self.L = int(f.readline()) # read in clock tree level
 			self.U = int(f.readline()) # read in sink number
-			for i in range(self.L):
-				self.num.append(int(f.readline())) # read in branch number of each level
-			for i in range(self.L):
+
+			self.branch.append(1)
+			for _ in range(self.L):
+				self.branch.append(int(f.readline())) # read in branch number of each level
+			for _ in range(self.L+1):
 				self.WL.append(float(f.readline())/1000) # read in wire length between every branchs(um)
 
+			for i in range(self.L+1):
+				fanout = 1
+				fanout_list = []
+				for j in range(i,self.L+1):
+					fanout *= self.branch[j]
+					fanout_list.append(fanout)
+				self.num.append(fanout_list)
+
+
+
 	def prepareLut(self):
-		DModel, SModel, PModel = load.initialize(5) # with buffer size
+		DModel, SModel = load.initialize()
 		# DModel is a list containing miu and sigma of delay at rise and fall edge respectively
 		# DModel[0]:miu(delay), rise edge | DModel[1]:sigma(delay), rise edge | DModel[2]:miu(delay), fall edge | DModel[3]:sigma(delay), fall edge
 
@@ -42,50 +70,56 @@ class Fitness:
 
 		# PModel is a list containing power value at rise and fall edge respectively
 		# PModel[0]:power dissiption, rise edge | PModel[1]:power dissiption, fall edge
-		self.f21 = SModel[2]
-		self.f22 = SModel[3]
-		self.f31 = DModel[2]
-		self.f32 = DModel[3]
+		self.f21 = SModel[0]
+		self.f22 = SModel[1]
+		self.f31 = DModel[0]
+		self.f32 = DModel[1]
 
-	def computeObj(self):
+	def computeObj(self,strategy):
 
-		# record index in strategy where not equal to 0
-		buffer_list = self.strategy
-		M = len(buffer_list) # buffer level in the strategy
+		
+		raw_strategy = np.array([self.lib_size] + strategy) # assume largest buffer at clock root
+
+		buffer_strategy = raw_strategy[raw_strategy != 0] - 1
+		buffer_index_of_strategy = np.nonzero(raw_strategy)[0]
+
+		M = buffer_strategy.size # buffer level in the strategy
 
 
-		Cl   = [0]*M
-		Slout = [0]*M
-		Slout_sigma = [0]*M
-		Delay = [0]*M
-		Delay_sigma = [0]*M
-		# breakpoint()
+		Cl   = np.zeros(M)
+		Slout = np.zeros(M)
+		Delay = np.zeros(M)
+		Delay_sigma = np.zeros(M)
 		# recording information of each buffer level
 
 		# construct load capacitance list of each clock buffer
-		for i in range(M):
-			if i < M -1:
-				Cl[i] = (self.WL[i]*self.c0 + self.f1[buffer_list[i+1]])*self.num[i]
-			else:
-				Cl[i] = (self.WL[i]*self.c0 + self.sink_cap)*self.num[i]
+
+		for i in range(M-1):
+			num = np.array(self.num[buffer_index_of_strategy[i]][:(buffer_index_of_strategy[i+1]-buffer_index_of_strategy[i])])
+			wl = np.array(self.WL[buffer_index_of_strategy[i]:buffer_index_of_strategy[i+1]])
+
+			Cl[i] = sum(num*wl*self.c0) + self.f1[buffer_strategy[i]]*num[-1]
+
+		num = np.array(self.num[buffer_index_of_strategy[-1]])
+		wl = np.array(self.WL[buffer_index_of_strategy[-1]:])
+		Cl[M-1] = sum(num*wl*self.c0) + self.sink_cap*num[-1]
 		slew_in = self.Slin
 		# tranverse clock path
 		for i in range(M):
-			s = buffer_list[i]
+			s = buffer_strategy[i]
 			Slout[i] = self.f21[s](slew_in, Cl[i])		
-			Slout_sigma[i] = self.f22[s](slew_in, Cl[i])
 			Delay[i] = self.f32[s](slew_in, Cl[i])
 			Delay_sigma[i] = self.f31[s](slew_in, Cl[i])
 			
 			slew_in = Slout[i]
 			
+		sigma_skew = np.power(Delay_sigma,2).sum() + sum(self.rho_matrix[buffer_strategy[i],buffer_strategy[i+1]]*Delay_sigma[i]* Delay_sigma[i+1] for i in range(M-1))
 
-		sigma_skew = (sum([u**2 for u in Delay_sigma]) + sum([self.rho_f*Delay_sigma[i]* Delay_sigma[i+1] for i in range(M-1)])) #ignore mathematical constant
-		return sigma_skew + self.a*max(Slout_sigma) + self.b*max(Slout)
+		return sigma_skew 
 		# return sigma_skew 
 
 
-	def objFitness(self):
-		fitness = 1/float(self.computeObj())
+	def objFitness(self,strategy):
+		fitness = 1/float(self.computeObj(strategy))
 		return fitness
 
